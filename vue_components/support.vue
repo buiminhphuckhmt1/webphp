37409e6ad6dd460b5d07910596a67aff47cd3c12
chuvanlong <longbep2003@gmail.com>
2023-01-11 10:12:31 +0700

    first commit

---
+++ vue_components/support.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="gf-support-section">
+        <div class="container">
+            <div class="gf-support-wrapper">
+                <div class="support-text">
+                    <p>Bạn cần hỗ trợ?</p>
+                    <p class="text-big">
+                        Hãy để Greenfield
+                        tư vấn cho bạn
+                    </p>
+                </div>
+                <div class="support-link">
+                    <div class="support-link-item">
+                        <a href="javascript:;" title="">
+                            <img class="img-fluid" src="images/common/support-ic1.png" alt="">
+
+                            Đăng ký <br />
+                            tuyển sinh
+                        </a>
+                    </div>
+                    <div class="support-link-item">
+                        <a href="javascript:;" title="">
+                            <img class="img-fluid" src="images/common/support-ic2.png" alt="">
+
+                            Đăng ký <br />
+                            tham quan
+                        </a>
+                    </div>
+                    <div class="support-link-item">
+                        <a href="javascript:;" title="">
+                            <img class="img-fluid" src="images/common/support-ic3.png" alt="">
+
+                            Tư vấn <br />
+                            hỗ trợ
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
