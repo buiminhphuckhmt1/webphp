bca50c60d37d9b12dba8aaa28ea60d3ea65e04b6
chuvanlong <longbep2003@gmail.com>
2023-02-07 08:21:29 +0700

    update

---
+++ vue_components/article-blog.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="article-blog-news">
+		<div class="item-image">
+			<a href="javascript:;">
+				<img src="images/activity.png" alt="" class="img-fluid" />
+			</a>
+		</div>
+		<div class="item-content">
+			<a href="" class="item-name" title="">
+				Hội nghị kiện toàn Đội TNTN Khoa CNTT.
+			</a>
+		</div>
+	</div>
+</template>
